4e130a05f41016d88691c1561f2ba06d1246472f
Carter Tomlenovich <cartertom@tuta.io>
2021-01-05 20:23:35 -0500

    Added some of the basic set colour code

--- src/main/java/dev/hyperlisk/chatmaster/commands/SetColour.java
+++ src/main/java/dev/hyperlisk/chatmaster/commands/SetColour.java
@@ -1,4 +1,30 @@
 package dev.hyperlisk.chatmaster.commands;
 
-public class SetColour {
+import dev.hyperlisk.chatmaster.ChatMaster;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetColour implements CommandExecutor {
+
+    private ChatMaster instance = ChatMaster.getInstance();
+
+
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(!(commandSender instanceof Player)) {
+            return false;
+        }
+
+        Player player = (Player) commandSender;
+        if(instance.getConfig().get("chatmaster.supporters").equals(player.getUniqueId())) {
+
+
+        } else {
+            player.sendMessage("[ChatMaster] Contact an admin to receive supporter status.");
+        }
+
+        return true;
+    }
+
 }
